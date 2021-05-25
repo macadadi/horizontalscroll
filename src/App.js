e60d74ecfe577db306240e12ca42d2401273cb1b
macadadi <macadadi36@gmail.com>
2021-05-25 14:17:48 +0300

    managed to create an horizontal scroll on wheel after failing to find the one that could certisfy my needs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+
+   const pk = useRef()
+  const handleweel =(e)=>{
+ 
+    if (e.deltaY > 0){pk.current.scrollLeft += 100;
+      console.log('scrolling left')} 
+    else{pk.current.scrollLeft -= 100;
+      console.log('scrolling right')} 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class='mmain ' ref={pk}style={{overflow: 'scroll hidden'}} onWheel={handleweel}>
+  <div class="wrapper" >
+     <div style={{backgroundColor:'red'}}>div 1</div>
+     <div style={{backgroundColor:'blue'}}>div 2</div>
+     <div style={{backgroundColor:'red'}}>div 3</div>
+     <div style={{backgroundColor:'blue'}}>div 4</div>
+     <div style={{backgroundColor:'red'}}>div 5</div>
+     <div style={{backgroundColor:'blue'}}>div 6</div>
+     <div style={{backgroundColor:'red'}}>div 7</div>
+     <div style={{backgroundColor:'blue'}}>div 8</div>
+  </div>
+</div>
     </div>
   );
 }
 
 export default App;
+
+
